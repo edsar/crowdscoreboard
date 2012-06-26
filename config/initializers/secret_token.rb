5d37ccb2b66c40957fa80f57fb8efaac0c198695
Rebecca <somerset@gmail.com>
2012-06-26 08:10:56 -0700

    Initial commit, empty project with postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowdscoreboard::Application.config.secret_token = '7b85aac7121542e53e34fe05fbc1388b585b274468d3ce1d7fe52f787b6034555ae278c488225be6c8d1f4c341a9a2ae3efa85724bd607869b62e13d0fc739d0'
