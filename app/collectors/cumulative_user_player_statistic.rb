f33601397de454a22e91dcef53e0a36690d07a3f
Rebecca <somerset@gmail.com>
2012-07-08 08:59:47 -0700

    commit another checkpoint with the calculation

--- app/collectors/cumulative_user_player_statistic.rb
+++ app/collectors/cumulative_user_player_statistic.rb
@@ -1,18 +1,17 @@
 class CumulativeUserPlayerStatistic
   
-  # def initialize(player_id,stat_id)
-  #     @count=0
-  #     @player_id=player_id
-  #     @stat_id = stat_id
-  #   end
-  
-  def initialize
-    @count=0
-  end
-  
+  def initialize(player_id,stat_id)
+        @count=0
+        @player_id=player_id
+        @stat_id = stat_id
+      end
     
     def increment
       @count = @count+1
+    end
+    
+    def count
+      @count
     end
    
 end
--- app/collectors/statistics_collector.rb
+++ app/collectors/statistics_collector.rb
@@ -22,11 +22,7 @@ class StatisticsCollector
     
     # we want to increment the count on this user's report for this stat type, player
     stat = collected_stats_map[user_id]
-    # @player_id = player_id
-    #    @stat_id = stat_id
-    #    puts "PID #{@player_id}"
-    #stat = CumulativeUserPlayerStatistic.new(1,2) unless stat
-    stat=CumulativeUserPlayerStatistic.new unless stat
+    stat = CumulativeUserPlayerStatistic.new(player_id,stat_id) unless stat
     stat.increment
     collected_stats_map[user_id]=stat
     game_map[rk]=collected_stats_map
@@ -35,24 +31,100 @@ class StatisticsCollector
     stat
   end
   
-  # def self.calculate_stats(game_id)
-  #    gk = game_key(game_id)
-  #    game_map = Rails.cache.read(gk)
-  #    return unless game_map
-  #    
-  #    game_map.keys.each do |key|
-  #      collected_stats_map = game_map[key]
-  #      puts key
-  #    end
-  #    
-  #  end
-  
-  def self.game_key(game_id)
-    return "game_stats_map." << game_id.to_s 
+  def self.get_map(game_id)
+     gk = game_key(game_id)
+     game_map = Rails.cache.fetch(gk){Hash.new}
   end
   
-  def self.record_key(user_id,game_id,team_id,player_id,stat_id)
-    game_id.to_s << "." << team_id.to_s << "." << player_id.to_s  << "." << stat_id.to_s
+  def self.calculate_stats(game_id)
+      gk = game_key(game_id)
+      game_map = Rails.cache.read(gk)
+      return unless game_map
+      
+      # we're going to sum it up by team while we're at it
+      teams_map=Hash.new
+      
+      #iterate through keys, which essential iterates over player/statistic type pairs
+      game_map.keys.each do |key|
+      counts = Array.new
+      # for each player get all the counts that have been reported
+      collected_stats_map = game_map[key]
+        collected_stats_map.values.each do |x| 
+        counts << x.count
+      end
+      # choose the most popular count reported
+      calculated_count = mode(counts)  
+      stat = CalculatedGamePlayerStatistic.find_or_create_by_statistic_type_id_and_game_id_and_player_id_and_team_id(
+        statistic_type_id(key),game_id(key),player_id(key),team_id(key))
+      stat.count = calculated_count
+      stat.save!
+      
+      # add that count to the total for this team and stat type
+      team_map = teams_map[team_id(key)] 
+      team_map = Hash.new unless team_map
+      team_record_key = team_id(key).to_s << "." <<  statistic_type_id(key).to_s 
+      entry = team_map[team_record_key]
+      entry = 0 unless entry
+      entry = entry + calculated_count
+      team_map[team_record_key]=entry
+      teams_map[team_id(key)]=team_map
+    end
+    
+    # create/update all the team records 
+    teams_map.keys.each do |team_id|
+      team_map=teams_map[team_id]
+        team_map.keys.each do |key|
+          team_id = key.split(".")[0]
+          statistic_type_id =  key.split(".")[0]
+          stat = CalculatedGameStatistic.find_or_create_by_statistic_type_id_and_game_id_and_team_id(
+              statistic_type_id,game_id,team_id)
+          stat.count = team_map[key]
+          stat.save!
+                
+        end
+    end  
+      
+  end
+    
+  def self.mode(x)
+     sorted = x.sort
+     a = Array.new
+     b = Array.new
+
+     sorted.each do |x| 
+        if a.index(x) == nil 
+           a << x 
+           b << 1 
+           else b[a.index(x)] +=1 
+         end 
+      end
+      
+     maxval = b.max 
+     where = b.index(maxval) 
+     return a[where] 
+  end
+     
+  def self.game_key(game_id) return "game_stats_map." << game_id.to_s end
+   
+  def self.record_key(user_id,game_id,team_id,player_id,stat_id) game_id.to_s << "." <<
+   team_id.to_s << "." << player_id.to_s << "." << stat_id.to_s 
+  end
+  
+
+  def self.statistic_type_id(recordkey)
+    recordkey.split(".")[3]
+  end
+
+  def self.player_id(recordkey)
+    recordkey.split(".")[2]
+  end
+  
+  def self.team_id(recordkey)
+    recordkey.split(".")[1]
+  end
+  
+  def self.game_id(recordkey)
+    recordkey.split(".")[0]
   end
   
   def self.update_game(game_id)
@@ -66,8 +138,5 @@ class StatisticsCollector
     game_hash[game_id]
   end 
   
-  def self.sayhi
-    return "hello"
-  end
   
 end
