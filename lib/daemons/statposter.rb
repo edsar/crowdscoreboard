0a01ca89ccc0ab3a328e6860d091bf7710a1ec89
Rebecca <somerset@gmail.com>
2012-08-04 10:04:04 -0700

    simplify tester daemon

--- lib/daemons/statposter.rb
+++ lib/daemons/statposter.rb
@@ -17,22 +17,22 @@ while($running) do
   Rails.logger.auto_flushing = true
   Rails.logger.info "This daemon is still running at #{Time.now}.\n"
 
-  begin
-  player = Player.all.sample
-  game = Game.all.sample
-  tr = TweetRecord.new
-  tr.user_screen_name="rebeccag_dev"
-  tr.user_twitter_id=1234567890123
-  tr.status_text="@c2sb #g#{game.id}p#{player.id}sFGM"
-  Rails.logger.info "generated tweet #{tr.inspect}"
-  #TweetCollector.add_tweet(tr)
-  #StatisticsCollector.add_tweet(68,"#g17p#{player.id}sFGM")
-  #Rails.logger.info "Submitted tweet for player #{player.id} - #{player.name}"
-  #Rails.logger.info("Tweet log #{TweetCollector.get_tweet_log.first.inspect}")
-  #@c2sb #g17p226sFGM
-  rescue => ex
-    Rails.logger.info "Exception #{ex.backtrace}"
-  end
+  #begin
+  #player = Player.all.sample
+  #game = Game.all.sample
+  #tr = TweetRecord.new
+  #tr.user_screen_name="rebeccag_dev"
+  #tr.user_twitter_id=1234567890123
+  #tr.status_text="@c2sb #g#{game.id}p#{player.id}sFGM"
+  #Rails.logger.info "generated tweet #{tr.inspect}"
+  ##TweetCollector.add_tweet(tr)
+  ##StatisticsCollector.add_tweet(68,"#g17p#{player.id}sFGM")
+  ##Rails.logger.info "Submitted tweet for player #{player.id} - #{player.name}"
+  ##Rails.logger.info("Tweet log #{TweetCollector.get_tweet_log.first.inspect}")
+  ##@c2sb #g17p226sFGM
+  #rescue => ex
+  #  Rails.logger.info "Exception #{ex.backtrace}"
+  #end
   #StatisticsCollector.add_tweet(68,"#g17p#{player.id}sFGM")
   #Rails.logger.info "Submitted tweet for player #{player.id} - #{player.name}"
 
